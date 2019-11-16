914eb0ff2661dc83c0f5f408d11729a795dfe013
JRSil <iystech@outlook.com>
2019-11-15 23:15:57 -0300

    Mais ajustes nos controllers

--- src/main/java/br/com/cinemajava/webcinema/controllers/AtendenteController.java
+++ src/main/java/br/com/cinemajava/webcinema/controllers/AtendenteController.java
@@ -3,23 +3,25 @@ package br.com.cinemajava.webcinema.controllers;
 import br.com.cinemajava.webcinema.models.Atendente;
 import br.com.cinemajava.webcinema.repository.AtendenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class AtendenteController
 {
     @Autowired
     private AtendenteRepository ar;
 
     @RequestMapping(value = "/cadastrarAtendente", method = RequestMethod.GET)
-    public String form()
+    public String formAtendente()
     {
         return "atendente/formAtendente";
     }
 
     @RequestMapping(value = "/cadastrarAtendente", method = RequestMethod.POST)
-    public String form(Atendente atendente)
+    public String formAtendente(Atendente atendente)
     {
         ar.save(atendente);
         return "redirect:/cadastrarAtendente";
